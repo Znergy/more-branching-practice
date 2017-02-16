6347faae576d2815682d5e1b31712bab3f1a951f
Dallas Slaughter and Ryan Jones <dallas@>
2017-02-16 10:54:03 -0800

    add a foreach loop to tally up the score

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,18 @@
-// var contact = ["firstName", "lastName", "addressStreet", "addressCity", "addressState", "addressZip","email", "phoneNumber"];
-//
-//      contact.forEach(function(info) {
-//      if (contact != null)
-//        var userAddress = $("input#" + info).val();
-//        $("." + info).text(userAddress).val();
-//        }
-//        else
-//        {
-//        forEach(function(info){
-//         $("."+info).addClass("whateverclassitwas");
-//        })
-//        }
 $(document).ready(function() {
+  var score = 0; // global
   $(".formOne").submit(function() {
     event.preventDefault();
-    alert("hwhwwj");
-    var checkBoxes = ["red", "blue", "green", "dog", "cat", "young", "middleAge", "old"];
-    checkBoxes.forEach(function(answer) {
-      var userChoice = $("#" + answer).val();
-      if ((".formOne"*:checked) {
-      console.log(userChoice + " user choice");
-      console.log(answer + " answer");
-    }
+
+    var choices = ["red", "blue", "green", "dog", "cat", "young", "middleAge", "old"];
+
+    choices.forEach(function(info) {
+      var userAnswer = $("#" + info).val();
+      var value = parseInt($(userAnswer).attr("value"));
+
+      if(userAnswer === true) {
+        score = score + value;
+      }
 
     });
   });
 });
-
-  // userChoice.forEach(function(){
-  //   if (answer === $("input:checkbox[name=flavor]:checked"))
-  // })
