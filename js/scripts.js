2d6942213145f410e9baa05f6a6c9f4093e293dc
Dallas Slaughter and Ryan Jones <ryan@>
2017-02-16 11:25:52 -0800

    Make forloop and if statement work correctly, adding score based on checkbox state.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
   var score = 0; // global
   $(".formOne").submit(function() {
+    console.log("score is, " + score);
     event.preventDefault();
 
     var choices = ["red", "blue", "green", "dog", "cat", "young", "middleAge", "old"];
 
     choices.forEach(function(info) {
-      console.log("start of foreach loop");
-      var userAnswer = $("#" + info).val();
-      console.log("this is user answer, " + userAnswer);
-      var value = parseInt($(userAnswer).attr("value"));
-      console.log("this is user value, " + value);
-      if(userAnswer === true) {
+      var userAnswer = ("#" + info).toString();
+      var value = parseInt($(userAnswer).val());
+
+      // var value = $(userAnswer).attr("value");
+      var name = $(userAnswer).attr("name");
+      var isChecked = $(userAnswer).is(':checked');
+      console.log(name +" is, " + isChecked);
+      if (isChecked === true) {
         console.log("in the if loop, the score is " + score);
         score = score + value;
         console.log("new score is, " + score);
       }
-
+      console.log("asdfnew score is, " + score);
     });
   });
 });
