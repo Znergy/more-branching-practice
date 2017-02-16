7c2a89ab8d817f532d1dc50850e180d8ca96e216
Dallas Slaughter and Ryan Jones <dallas@>
2017-02-16 10:57:08 -0800

    add console.log to test javascript

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,15 @@ $(document).ready(function() {
     var choices = ["red", "blue", "green", "dog", "cat", "young", "middleAge", "old"];
 
     choices.forEach(function(info) {
+      console.log("start of foreach loop");
       var userAnswer = $("#" + info).val();
+      console.log("this is user answer, " + userAnswer);
       var value = parseInt($(userAnswer).attr("value"));
-
+      console.log("this is user value, " + value);
       if(userAnswer === true) {
+        console.log("in the if loop, the score is " + score);
         score = score + value;
+        console.log("new score is, " + score);
       }
 
     });
