faf2e08de732662e82dc9b5044d772d6d0dc4c45
Dallas Slaughter and Ryan Jones <ryan@>
2017-02-16 11:57:20 -0800

    add logic , some css, columns in html, and ammend

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
   var score = 0; // global
   $(".formOne").submit(function() {
-    console.log("score is, " + score);
     event.preventDefault();
-
+    score = 0;
     var choices = ["red", "blue", "green", "dog", "cat", "young", "middleAge", "old"];
 
     choices.forEach(function(info) {
       var userAnswer = ("#" + info).toString();
       var value = parseInt($(userAnswer).val());
-
-      // var value = $(userAnswer).attr("value");
-      var name = $(userAnswer).attr("name");
       var isChecked = $(userAnswer).is(':checked');
-      console.log(name +" is, " + isChecked);
+
       if (isChecked === true) {
-        console.log("in the if loop, the score is " + score);
         score = score + value;
-        console.log("new score is, " + score);
       }
-      console.log("asdfnew score is, " + score);
     });
+
+    if (score <= 5 && score > 0) {
+      console.log("the score is " + score);
+      $(".neilTyson").show();
+    } else if (score > 5 && score <= 10) {
+      $(".carlSagan").show();
+    } else if (score > 10) {
+      $(".richardDawkins").show();
+    } else {
+      $(".pickSomething").show();
+    }
   });
 });
